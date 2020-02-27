7865930c90a35d054949ce4e2f436f67b2cec40a
Beryl01 <berylnegesa@gmail.com>
2020-02-27 21:23:54 +0300

    changes on my js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var myIndex = 0;
+carousel();
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 function Order(type, size, crust, topping) {
   this.type = type;
   this.size = size;
@@ -6,20 +20,24 @@ function Order(type, size, crust, topping) {
 }
 
 Order.prototype.fullOrder = function() {
-  return this.type + " with the topping of " + this.topping + " and " + this.crust + " as crust";
+  return `${this.type} size ${this.size} and ${this.crust} as crust ${this.topping} as topping`
 };
 
-function Total(price, quantity, delivery) {
+function Total(price, quantity, crust, topping, delivery) {
   this.price = price;
   this.quantity = quantity;
+  this.crust = crust;
+  this.topping = topping;
   this.delivery = delivery; 
 }
 
 Total.prototype.finalTotal = function() {
-  return this.price * this.quantity + this.delivery;
+  return (this.price + this.crust + this.topping)* this.quantity + this.delivery;
 };
 
 var sizePrice = [1200, 1000, 800, 550];
+var crustPrices = [50, 55, 60, 70, 100];
+var toppingPrices = [100, 110, 120, 130, 140, 150, 160];
 var deliveryPrices = [0, 300];
 
 $(document).ready(function() {
@@ -27,15 +45,17 @@ $(document).ready(function() {
     event.preventDefault();
     var pizzaType = $('#type').val();
     var pizzaSize = parseInt($('#size').val());
-    var pizzaCrust = $('#crust').val();
-    var pizzaTop = $('#top').val();
+    var pizzaCrust = parseInt($('#crust').val());
+    var pizzaTop = parseInt($('#top').val());
     var pizzaQty = parseInt($('#qty').val());
     var pizzaPick = parseInt($('#pick').val());
     var price = sizePrice[pizzaSize - 1];
+    var crustCost = crustPrices[pizzaCrust - 1];
+    var toppingCost = toppingPrices[pizzaTop - 1];
     var deliveryCost = deliveryPrices[pizzaPick - 1];
 
     newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
-    newTotal = new Total(price, pizzaQty, deliveryCost);
+    newTotal = new Total(price, pizzaQty, toppingCost, crustCost, deliveryCost);
     if (pizzaPick === 1) {
       document.getElementById("specs").innerHTML =
       `Your order is ${newOrder.fullOrder()}.`
@@ -61,7 +81,7 @@ $(document).ready(function() {
     var email = $('#email').val();
     var message = $('#comment').val();
     document.getElementById("thank").innerHTML =
-    "Hello " + name + " .Thank You for Contacting Us.";
+    `Hello ${name}.Thank You for Contacting Us.`
   });
 });
 });
